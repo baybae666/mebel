462414763f71d4d039ce06b7cf869ee286b56cc0
xxxioan <kraskafet@gmail.com>
2025-05-18 16:21:44 +0500

    Update OrderConfirm.js

--- client/src/pages/OrderConfirm.js
+++ client/src/pages/OrderConfirm.js
@@ -7,7 +7,7 @@ import {useNavigate} from "react-router-dom";
 import {PROFILEROUTER} from "../utils/consts";
 
 const OrderConfirm = () => {
-    const {orderStore} = useContext(Context)
+    const {orderStore, userStore} = useContext(Context)
     const navigate = useNavigate()
     // const checkOrder = () => orderStore.order.OrderID ? navigate(PROFILEROUTER + '/orders)' : ''
      useEffect(() => {
