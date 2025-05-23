cdd779d87c060e883d02c9670ae085e38ec03b6c
lfyz12 <kraskafet@gmail.com>
2025-05-24 00:25:23 +0500

    cookies v2

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const cookieParser = require('cookie-parser')
 const PORT = process.env.PORT || 3000
 const errorMiddleware = require('./middleware/errorMiddleware');
 const corsOptions ={
-    origin:'*',
+    origin:process.env.CLIENT_URL,
     credentials:true,
     optionSuccessStatus:200
 }
